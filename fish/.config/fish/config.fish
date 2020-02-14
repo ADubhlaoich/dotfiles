73081a8a178d55481974a4718439184b1fdc8ade
Alan Dooley <alandubhlaoich@gmail.com>
2020-02-14 20:50:11 +0000

    Add dunst and fish configuration files
    A recent reformat has changed up my stack a little; I didn't realise I
    hadn't been versioning my fish config either. Nothing too fancy yet, but
    still some time lost when setting back up.

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,14 @@
+# Install fisher if it does not exist
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+# Autostart X on login
+
+if status is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR = 1
+        exec startx -- -keeptty
+    end
+end
